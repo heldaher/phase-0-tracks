405acab875237d9586dcea897e19bd03a9bf1115
Henri El Daher <henrieldaher@gmail.com>
2016-06-18 11:03:22 -0400

    almost complete challenge, still struggling with loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,28 @@
+#Start of program
+puts "How many employees will be processed?"
+	employees_to_process = gets.chomp
+	counter_variable = 0
+
+
+#loop on vampire questions
+until 
+	counter_variable.to_i == employees_to_process.to_i
+
+
+#loop on allergies
+puts "Please name any allergies (type 'done' when finished)"
+	allergy = gets.chomp
+until 
+	allergy == "done" || "sunshine"
+	puts "Please name any allergies (type 'done' when finished)"
+	allergy = gets.chomp
+end
+
+if 
+	allergy == "done"
+
+
+(
 #Ask questions
 puts "What is your name"
 	user_name = gets.chomp
@@ -38,12 +63,31 @@ if
 	vampire_conclusion = "Definitely a vampire"
 end
 
-p vampire_conclusion
+if
+	vampire_conclusion = "Probably not a vampire" ||
+	"Probably a vampire" ||
+	"Almost certainly a vampire" ||
+	"Definitely a vampire"
+	p vampire_conclusion
+else
+	puts "Results inconclusive"
+end
+)
+
+##sunshine skip
+elsif
+	allergy == "sunshine"
+	puts "Probably a vampire"
+else
+end
+
+#end loop
+	counter_variable += 1
+end
 
 
-
-
-
+#plot twist
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
 
