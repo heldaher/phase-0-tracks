cd6a4ab98e3102fbcdd1f795fc1a7b30e4f7bc3d
Henri El Daher <henrieldaher@gmail.com>
2016-06-27 23:45:23 -0400

    finish release 0 of challenge 6-1

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# 
+# SPECIES ------------------------
+# Puppy
+# 
+# CHARACTERISTICS ----------------
+# Cuteness level: off the charts
+# Friendly: yes
+# Leg count: 4
+# Eye count: 2
+# Name: varies
+# Color: varies
+# 
+# BEHAVIOR -----------------------
+# Run
+# Hug
+# Eat
+# Fetch
+# Bark
