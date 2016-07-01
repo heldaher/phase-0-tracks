e850ce4a22f1ceabcfbbcf5081d0fe5c9923dad8
Henri El Daher <henrieldaher@gmail.com>
2016-06-30 23:58:08 -0400

    almost complete release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,11 @@
+reindeer_ranking = [
+	"Rudolph", "Dasher", "Dancer", "Prancer", 
+	"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+p reindeer_ranking
+
+age = 0
+
 class Santa
 
 	def initialize (gender, ethnicity, beard_description, weight)
@@ -16,13 +24,36 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
+	#getter
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	#setter
+	def celebrate_birthday(age)
+		@age = age + 1
+	end
+
+#not sure how to get this to work
+	def get_mad_at=name
+	#	reindeer_ranking.insert(8,reindeer_ranking.delete_at(reindeer_ranking.index(name)))
+		reindeer_ranking = [
+		"Rudolph", "Dasher", "Dancer", "Prancer", 
+		"Vixen", "Comet", "Cupid", "Donner", "Blitzen"].push(name)
+
+		#reindeer_ranking.push(name)
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
 end
 
-reindeer_ranking = [
-	"Rudolph", "Dasher", "Dancer", "Prancer", 
-	"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-
-age = 0
 
 #Driver code - release 0
 #sugar = Santa.new
@@ -30,21 +61,40 @@ age = 0
 #sugar.eat_milk_and_cookies("sugar cookie")
 
 #Driver code - release 1
-santas = []
-example_genders = ["agender", "female", "bigender", 
-	"male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", 
-	"Japanese-African", "prefer not to say", 
-	"Mystical Creature (unicorn)", "N/A"]
-example_beards = ["white", "poofy", "mustache only", 
-	"cool hipster", "the Moses", "stubble", "N/A"]
-example_weights = ["very large", "large", "one too many cookies", 
-	"five too many cookies", "large", "avid soul cyclist", 
-	"should eat more cookies!"]
+#santas = []
+#example_genders = ["agender", "female", "bigender", 
+#	"male", "female", "gender fluid", "N/A"]
+#example_ethnicities = ["black", "Latino", "white", 
+#	"Japanese-African", "prefer not to say", 
+#	"Mystical Creature (unicorn)", "N/A"]
+#example_beards = ["white", "poofy", "mustache only", 
+#	"cool hipster", "the Moses", "stubble", "N/A"]
+#example_weights = ["very large", "large", "one too many cookies", 
+#	"five too many cookies", "large", "avid soul cyclist", 
+#	"should eat more cookies!"]
 
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i],
-  	example_beards[i], example_weights[i])
-end
+#example_genders.length.times do |i|
+#  santas << Santa.new(example_genders[i], example_ethnicities[i],
+#  	example_beards[i], example_weights[i])
+#end
+#
+#p santas
 
-p santas
+#Release 2 driver code
+santa = Santa.new("male", "korean", "poofy", "eat more cookies")
+#puts "#{santa.ethnicity} Santa is not very old at the young age of 
+	#{santa.celebrate_birthday(age)}"
+#santa.get_mad_at("henri")
+#p reindeer_ranking
+
+#reindeer_ranking.push("henri")
+#p reindeer_ranking
+
+p santa
+santa.gender = "female"
+p santa
+
+
+
+
+
