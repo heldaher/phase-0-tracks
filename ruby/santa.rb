876908b4217b6e605757b850692b1d4fa66f5d1b
Henri El Daher <henrieldaher@gmail.com>
2016-06-30 21:13:26 -0400

    complete release 0 and 1

---
+++ ruby/santa.rb
@@ -0,0 +1,50 @@
+class Santa
+
+	def initialize (gender, ethnicity, beard_description, weight)
+		#puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@beard_description = beard_description
+		@weight = weight
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaapy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type) 
+		puts "That was a good #{cookie_type}!"
+	end
+
+end
+
+reindeer_ranking = [
+	"Rudolph", "Dasher", "Dancer", "Prancer", 
+	"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+age = 0
+
+#Driver code - release 0
+#sugar = Santa.new
+#sugar.speak
+#sugar.eat_milk_and_cookies("sugar cookie")
+
+#Driver code - release 1
+santas = []
+example_genders = ["agender", "female", "bigender", 
+	"male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", 
+	"Japanese-African", "prefer not to say", 
+	"Mystical Creature (unicorn)", "N/A"]
+example_beards = ["white", "poofy", "mustache only", 
+	"cool hipster", "the Moses", "stubble", "N/A"]
+example_weights = ["very large", "large", "one too many cookies", 
+	"five too many cookies", "large", "avid soul cyclist", 
+	"should eat more cookies!"]
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i],
+  	example_beards[i], example_weights[i])
+end
+
+p santas
