2b6b87ca27f50739cdea2e06bc6aa5a6e2029414
Henri El Daher <henrieldaher@gmail.com>
2016-07-01 15:17:41 -0400

    complete challenge 6-5

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,40 @@
+#Release 0 and 1
+
+#module Shout
+#  def self.yell_angrily(words)
+#  	words + "!!!" + " :("
+#  end
+#
+#  def self.yelling_happily(words)
+#  	words + "hohohoho" + ":)"
+#  end
+#end
+
+#require_relative 'shout'
+#p Shout.yelling_happily("hey")
+#p Shout.yell_angrily("hello")
+
+#Release 3
 module Shout
-  def self.yell_angrily(words)
-  	words + "!!!" + " :("
+  def yell_angrily(words)
+    words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
-  	words + "hohohoho" + ":)"
+  def yelling_happily(words)
+  	words + " woohoo" + ":)"
   end
 end
 
-#require_relative 'shout'
-p Shout.yelling_happily("hey")
-p Shout.yell_angrily("hello")
+class Coach
+	include Shout
+end
 
+class Sargeant
+	include Shout
+end
+
+coach = Coach.new
+p coach.yelling_happily("good job team")
+
+sargeant = Sargeant.new
+p sargeant.yell_angrily("Attention")
