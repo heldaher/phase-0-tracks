10a4df1004d15229d939b5b2613802104fa6c5ec
Henri El Daher <henrieldaher@gmail.com>
2016-07-07 15:59:54 -0400

    complete first test of four

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,3 +1,15 @@
 class TodoList
+	attr_accessor :list_array
 
+	def initialize(item1, item2)
+		@item1 = item1
+		@item2 = item2
+	end
+
+	def get_items
+		@list
+	end
 end
+
+list_one = TodoList.new("walk the dog", "mow the lawn")
+p list_one
