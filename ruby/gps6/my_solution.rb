b2d23993cedd36295caf5374703c31b380ad065c
Henri El Daher <henrieldaher@gmail.com>
2016-07-11 21:00:22 -0400

    review code and add comments

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,25 +4,30 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# The require_relative grabs the code from the file in quotations 
+# Require_relative is used when the file provided is in the same folder, whereas require needs more information on the folder from which to grab the .rb file
 require_relative 'state_data'
 
 class VirusPredictor
 
+#initialize method which takes variabes upon creating an instance of the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+#Calls two methods
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
+#We guess that private means methods within private cannot be used outside of the private block
   private
 
+#takes three variables and applies conditional flow logic to predict number of deaths
+#we guees .floor method will round values down
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,6 +46,8 @@ class VirusPredictor
 
   end
 
+#this method takes two variables and applies conditional flow logic to predict the speed of the spread of the disease
+#will output string with speed variable interpolated and the speed variable is being calculated within the method
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -69,7 +76,8 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-
+#Driver code creates new instances of the VirusPredictor class and includes three arguments
+#first argument is the state of origin, second argument and third argument point to the figures in the state_date file (specifically the value within another value (i.e., nested data structure))
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
 
