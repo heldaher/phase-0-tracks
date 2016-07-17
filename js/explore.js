5f0ab938eeb670f15572025ff26c455ca77acd3c
Henri El Daher <henrieldaher@gmail.com>
2016-07-17 12:53:59 -0400

    complete challenge 7.1 to reverse string

---
+++ js/explore.js
@@ -0,0 +1,20 @@
+// Write a for loop which counts down from the string length one
+// at a time, and gives the index of the string starting from the 
+// last letter of the string
+
+function reverse(str) {
+	for (var i = (str.length - 1); i >= 0; i-=1) 
+	{console.log(str[i]);}
+	}
+
+// Driver code
+// declare boolean variable
+var henri26 = true;
+
+// declare new variable and print if condition is true
+if (henri26 == true) {
+	var greeting_reverse = reverse("hello");
+}
+
+
+
