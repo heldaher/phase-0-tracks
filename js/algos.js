f2cf6bbc44c080d8f76020490941dcf210f639ef
Henri El Daher <henrieldaher@gmail.com>
2016-07-18 14:40:08 -0400

    made some changes to prior code and made progress on release 2

--- js/algos.js
+++ js/algos.js
@@ -27,17 +27,58 @@ function greatest_length(array) {
 // Write a for loop that:
 //   - FOR each key in an object, see if it is INCLUDED in the other object
 //   - IF compared items are equal, than return true
-//   - ELSEIF items are not queal, than do the same for value
+//   - ELSE IF items are not queal, than do the same for value
 //   - ELSE return false
 
-function share_pair(object1,object2) {
-	for (var i = 0; i < object1.length; i++)
-		{if ( (object2.indexOf(object1.key[i])) != null)
-			console.log("true");
-		elsif (object2.indexOf(object1.value[i]) != null)
-			return true;
-		else console.log("false");}
+// function sharePair(object1,object2) {
+// 	for (var i = 0; i < object1.length; i++)
+// 		{if ( (object2.indexOf(object1.key[i])) != null) {
+// 			console.log("true");
+// 		} else if (object2.indexOf(object1.value[i]) != null) {
+// 			console.log("true"); }
+// 		else {
+//			console.log("false");}
+// }
+
+// Release 2
+// Write a function that takes an integer for length,
+// and builds and returns an array of strings of the given length
+
+// Write a function called random_array
+// The function takes an integer as an argument
+// Declare a variable called alphabet that has each letter of the alphabet
+// Generate a random letter by randomply choosing a letter from the alphabet string
+// Randomly choose between 1 and 10 random characters
+// Add up each random letter chosen
+// Do this [integer] times
+
+var alphabet = "abcdefghij";
+var random_number = (Math.floor((Math.random() * 10) + 1));
+var random_word = ""
+var random_array_structure = []
+random_letter = alphabet[random_number];
+random_word = random_word + random_letter;
+
+function randomNumber() {
+  return (Math.floor((Math.random() * 10) + 1));
 }
+
+
+function randomArray(x) {
+  // First see if I can generate a random letter
+  for (var i = 0; i < x; i++)
+	{
+	  for (var ii = 0; ii <= random_number; i++) 
+	    {
+	    random_letter = alphabet[random_number];
+	    random_word = random_word + random_letter;
+		}
+	}
+  random_array_structure.push(random_word)
+}
+
+console.log(randomArray(4))
+	
 
 
 /////////////////////////////////////////////////////////////
@@ -48,11 +89,11 @@ function share_pair(object1,object2) {
 //greatest_length(["MJ","Kobe","Lebron"]);
 
 // Release 1 - would not work
-var bball_players = {nickname: "MJ", age: 52};
-var football_players = {name: "Maradonna", age: 52};
-console.log(share_pair(bball_players,football_players));
+// var bball_players = {nickname: "MJ", age: 52};
+// var football_players = {name: "Maradonna", age: 52};
+// console.log(sharePair(bball_players,football_players));
 
-
+// Release 2 
 
 
 
