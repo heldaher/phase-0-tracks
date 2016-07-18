f75443c65e72063b2b3aad87c33a810cf48766df
Henri El Daher <henrieldaher@gmail.com>
2016-07-18 15:54:09 -0400

    more work trying to get release 1 and 2 to work but did not succeed

--- js/algos.js
+++ js/algos.js
@@ -1,8 +1,9 @@
+// Release 0 - task
 // write a function that takes an array of words or phrases and returns the longest word or phrase in the array. 
 // So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], 
 // it would return "longest phrase".
 
-// Release 0
+// Release 0 - psuedocode
 // Create an empty array
 
 var new_array = [];
@@ -19,66 +20,71 @@ function greatest_length(array) {
 		console.log(Math.max.apply(Math, new_array));
 }
 
-// Release 1
+// Release 1 - task
 // write a function that takes two objects and checks 
 // to see if the objects share at least one key-value pair
 
-// Write a function called share_pair which takes two objects as argument
+// Release 1 - pseudocode
+// Write a function called sharePair which takes two objects as argument
 // Write a for loop that:
-//   - FOR each key in an object, see if it is INCLUDED in the other object
+//   - FOR each key in the 1st object, see if it is included (using indexOf) in the other object
 //   - IF compared items are equal, than return true
-//   - ELSE IF items are not queal, than do the same for value
+//   - ELSE IF do the same for each value (as opposed to key) in the 1st ojbect
 //   - ELSE return false
+// Note that this would not work
 
-// function sharePair(object1,object2) {
-// 	for (var i = 0; i < object1.length; i++)
-// 		{if ( (object2.indexOf(object1.key[i])) != null) {
-// 			console.log("true");
-// 		} else if (object2.indexOf(object1.value[i]) != null) {
-// 			console.log("true"); }
-// 		else {
-//			console.log("false");}
-// }
+function sharePair(object1,object2) {
+	for (var i = 0; i < object1.length; i++)
+		if ( (object2.indexOf(object1.keys[i])) != null) {
+			console.log("true");
+		} else if (object2.indexOf(object1.values[i]) != null) {
+			console.log("true");}
+		else {
+			console.log("false");}
+}
 
-// Release 2
+// Release 2 - task
 // Write a function that takes an integer for length,
 // and builds and returns an array of strings of the given length
 
-// Write a function called random_array
-// The function takes an integer as an argument
-// Declare a variable called alphabet that has each letter of the alphabet
-// Generate a random letter by randomply choosing a letter from the alphabet string
-// Randomly choose between 1 and 10 random characters
-// Add up each random letter chosen
-// Do this [integer] times
+// Release 2 - pseudocode
+// Create variables:
+//   - alphabet variable to take a string of the first 10 letters of the alphabet
+//   - random_number variable to take a random number between 1 and 10
+//   - random_word variable which will start off as an empty string but be populated later
+//   - random_array which will start off an empty array but be populated later
+//   - random_letter which will be a random index of the alphabet variable
 
 var alphabet = "abcdefghij";
 var random_number = (Math.floor((Math.random() * 10) + 1));
 var random_word = ""
-var random_array_structure = []
+var random_array = []
 random_letter = alphabet[random_number];
-random_word = random_word + random_letter;
 
+// Write a function called randomNumber to take a random number between 1 and 10
 function randomNumber() {
   return (Math.floor((Math.random() * 10) + 1));
 }
 
+// Write a function called randomWord to generate a string of random letters (thus a random word)
+// This would not properly work
+function randomWord() {
+  for (var y = 0; y <= randomNumber(); y++) {
+	random_letter = alphabet[randomNumber()]
+	}
+ 	return random_word = random_word + random_letter;
+}
+
+// Write a function called random_array
+// The function takes an integer as an argument
+// Write a for loop that calls the randomWord function [integer] times
 
 function randomArray(x) {
   // First see if I can generate a random letter
-  for (var i = 0; i < x; i++)
-	{
-	  for (var ii = 0; ii <= random_number; i++) 
-	    {
-	    random_letter = alphabet[random_number];
-	    random_word = random_word + random_letter;
-		}
-	}
-  random_array_structure.push(random_word)
+  for (var i = 0; i < x; i++) {
+	{randomWord};
+    return random_array.push(random_word)
 }
-
-console.log(randomArray(4))
-	
 
 
 /////////////////////////////////////////////////////////////
@@ -92,13 +98,9 @@ console.log(randomArray(4))
 // var bball_players = {nickname: "MJ", age: 52};
 // var football_players = {name: "Maradonna", age: 52};
 // console.log(sharePair(bball_players,football_players));
+// console.log(bball_players.length)
 
-// Release 2 
-
-
-
-
-
-
-
+// Release 2 - would not work
+randomArray(4);
+console.log(random_array);
 
