334a9e58bf3a643c8d789f2a12c468359c53b053
Henri El Daher <henrieldaher@gmail.com>
2016-07-27 15:59:51 -0400

    complete release 1

---
+++ web_dev/client_side_js/jscode.js
@@ -0,0 +1,12 @@
+console.log("test script")
+
+var els = document.getElementsByTagName("h1");
+var el = els[0];
+
+ function changeFontColor() {
+   var els = document.getElementsByTagName("h1");
+   var el = els[0];
+   el.style.color = "yellow";
+}
+
+changeFontColor()
