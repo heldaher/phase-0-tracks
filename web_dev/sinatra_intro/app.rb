585d52e617af055137528e906f3ca2349f2a0821
Henri El Daher <henrieldaher@gmail.com>
2016-07-31 14:49:44 -0400

    complete challenge 9-4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -23,6 +23,39 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
+#new contact route
+get '/contact' do
+  "2347 Hamilton Dr, Portland, 20413"
+end
+
+#new great job route
+#had an issue getting else to run when no name was provided
+get '/great_job/:name' do
+  name = params[:name]
+  if name
+    "Good job, #{name}"
+  else
+    "Good job!"
+  end
+end
+
+#use param to add 2 numbers and respond with result
+get '/:number_1/plus/:number_2' do
+  number_one = params[:number_1]
+  number_two = params[:number_2]
+  number_one_i = number_one.to_i
+  number_two_i = number_two.to_i
+  number_three = number_one_i + number_two_i
+  number_three.to_s
+end
+
+#bonus route attempt - did not work
+get '/students/:Name' do
+  student = db.execute("SELECT Name FROM students WHERE Name=?", [params[:Name]])
+  student.to_s
+end
+
+
 # write a GET route that retrieves
 # all student data
 get '/students' do
